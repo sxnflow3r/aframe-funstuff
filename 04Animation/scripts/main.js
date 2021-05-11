2a0a0695bd1c06b0900541f03f8a19d4700b840a
sxnflow3r <58250090+sxnflow3r@users.noreply.github.com>
2021-05-11 16:27:41 +0200

    playing with animation

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,13 +1,14 @@
 
 
 //changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+var rotationSpeed = 0.1;
+var rotationSpeed1 = 0.05;
  var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
  	myOtherBox.object3D.rotation.x -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
-	myOtherBox.object3D.rotation.y += rotationSpeed;
+	myOtherBox.object3D.rotation.y += rotationSpeed1;
 	console.log(myOtherBox.object3D.rotation.y);
  }
 
