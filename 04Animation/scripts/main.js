19b2b4583313aa41626f3652aaab51c8b6fcb528
sxnflow3r <58250090+sxnflow3r@users.noreply.github.com>
2021-05-11 16:22:26 +0200

    Update main Js by adding rotations

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,10 @@ var rotationSpeed = 0.01;
  var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
- 	myOtherBox.object3D.rotation.x += rotationSpeed;
+ 	myOtherBox.object3D.rotation.x -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation.y += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.y);
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
